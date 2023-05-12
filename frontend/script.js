34f733f45eb30e6b56ce58ceab109dcbe46720f2
Peipr <maro@peiphy.xyz>
2023-05-13 01:04:31 +0200

    Managed to get moving a singular point working

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,32 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const radius = 10;
 
-function drawPoint(x, y) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(x, y, 1, 1);
-}
+// set initial position
+let x = 50;
+let y = 50;
 
-canvas.addEventListener("click", (event) => {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    drawPoint(x, y);
+// draw circle at initial position
+ctx.beginPath();
+ctx.arc(x, y, radius, 0, 2 * Math.PI);
+ctx.fillStyle = 'blue';
+ctx.fill();
+
+// add event listener for mouse click
+canvas.addEventListener('click', (event) => {
+    // get click coordinates relative to canvas
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    // clear canvas and redraw circle at new position
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    x = mouseX;
+    y = mouseY;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
 });
+
+
