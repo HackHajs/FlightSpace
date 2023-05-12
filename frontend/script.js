5cdd121a0cc2a5bcbcd1ee8558b4d73a4e93c48a
Peipr <maro@peiphy.xyz>
2023-05-13 01:16:17 +0200

    Singular point animation

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const radius = 10;
+const radius = 25;
+const speed = 50; // pixels per frame
 
 // set initial position
 let x = 50;
@@ -16,17 +17,42 @@ ctx.fill();
 canvas.addEventListener('click', (event) => {
     // get click coordinates relative to canvas
     const rect = canvas.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
+    const targetX = event.clientX - rect.left;
+    const targetY = event.clientY - rect.top;
 
-    // clear canvas and redraw circle at new position
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    x = mouseX;
-    y = mouseY;
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
+    // calculate distance to target
+    const dx = targetX - x;
+    const dy = targetY - y;
+    const distance = Math.sqrt(dx*dx + dy*dy);
+
+    // calculate number of frames needed to reach target
+    const frames = Math.ceil(distance / speed);
+
+    // calculate x and y increments per frame
+    const incrementX = dx / frames;
+    const incrementY = dy / frames;
+
+    // start animation
+    let frame = 1;
+    const animate = () => {
+        // clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // update position
+        x += incrementX;
+        y += incrementY;
+
+        // draw circle at new position
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+
+        // check if animation is finished
+        if (frame < frames) {
+            frame++;
+            requestAnimationFrame(animate);
+        }
+    };
+    animate();
 });
-
-
