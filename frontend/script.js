49d5f1326c12f6d8c026afb051ca0bf4f4d9d6ae
Peipr <maro@peiphy.xyz>
2023-05-13 02:25:22 +0200

    Testing random animation stuffs

--- frontend/script.js
+++ frontend/script.js
@@ -8,11 +8,11 @@ let x = 50;
 let y = 50;
 
 // draw circle at initial position
+ctx.globalCompositeOperation = "source-over";
 ctx.beginPath();
 ctx.arc(x, y, radius, 0, 2 * Math.PI);
 ctx.fillStyle = 'blue';
 ctx.fill();
-
 // add event listener for mouse click
 canvas.addEventListener('click', (event) => {
     // get click coordinates relative to canvas
