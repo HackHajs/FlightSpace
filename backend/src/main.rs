43f94c332261775ec959b89672ff213e83c8ed0b
Slushee-a <slushee@duck.com>
2023-05-13 17:18:30 +0200

    Restore basic features

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,19 @@
-use std::{
-    collections::hash_map::HashMap,
-};
-
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use regex::Regex;
 
 mod players;
-use players::{parse_players, save_players, Message, Field};
+use players::{parse_players, save_players, Message, Player};
+
+const X_CENTER: i32 = 1920 / 2;
+const Y_CENTER: i32 = 1080 / 2;
 
 #[get("/players")]
 async fn get_players() -> impl Responder {
-    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin", "*")).json(Message{ players: parse_players() })
+    HttpResponse::Ok()
+        .insert_header(("Access-Control-Allow-Origin", "*"))
+        .json(Message {
+            players: parse_players(),
+        })
 }
 
 #[get("/update/{data}")]
@@ -19,25 +22,60 @@ async fn update_player(data: web::Path<String>) -> impl Responder {
     let cap = re.captures_iter(&data).next().unwrap();
     let player = cap[1].to_string();
     let field = &cap[2];
-    let value: u32 = cap[3].parse().unwrap();
+    let value: i32 = cap[3].parse().unwrap();
 
     let mut players = parse_players();
 
     match field {
         "health" => {
-            players.entry(player).and_modify(|player| player.health += value);
+            players
+                .entry(player)
+                .and_modify(|player| player.health += value);
         }
         "xpos" => {
-            players.entry(player).and_modify(|players| players.x_pos = value);
+            players
+                .entry(player)
+                .and_modify(|players| players.x_pos = value);
         }
         "ypos" => {
-            players.entry(player).and_modify(|players| players.y_pos = value);
+            players
+                .entry(player)
+                .and_modify(|players| players.y_pos = value);
         }
         _ => (),
     };
+    save_players(&players);
+
+    HttpResponse::Ok()
+        .insert_header(("Access-Control-Allow-Origin", "*"))
+        .json("OK")
+}
+
+#[get("/count")]
+async fn player_count() -> impl Responder {
+    HttpResponse::Ok()
+        .insert_header(("Access-Control-Allow-Origin", "*"))
+        .json(parse_players().len())
+}
+
+#[get("/join/{name}")]
+async fn create_player(name: web::Path<String>) -> impl Responder {
+    let mut players = parse_players();
+    players.insert(
+        name.to_string(),
+        Player {
+            name: name.to_string(),
+            health: 3,
+            x_pos: X_CENTER,
+            y_pos: Y_CENTER,
+        },
+    );
+
     save_players(players);
 
-    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin", "*")).json("OK")
+    HttpResponse::Ok()
+        .insert_header(("Access-Control-Allow-Origin", "*"))
+        .json("OK")
 }
 
 #[actix_web::main]
@@ -45,8 +83,14 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    HttpServer::new(|| App::new().service(get_players).service(update_player))
-        .bind("127.0.0.1:8080")?
-        .run()
+    HttpServer::new(|| {
+        App::new()
+            .service(get_players)
+            .service(update_player)
+            .service(player_count)
+            .service(create_player)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
     .await
 }
--- backend/src/players.rs
+++ backend/src/players.rs
@@ -1,31 +1,18 @@
 use std::{
-    fs::{ 
-        read_to_string,
-        write,
-        File,
-    },
-    io::{
-        ErrorKind,
-        prelude::*,
-    },
     collections::HashMap,
+    fs::{read_to_string, File},
+    io::{prelude::*, ErrorKind},
 };
 
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 
-pub enum Field {
-    Xpos(u32),
-    Ypos(u32),
-    Health(u8),
-}
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Player {
     pub name: String,
-    pub x_pos: u32,
-    pub y_pos: u32,
-    pub health: u32,
+    pub x_pos: i32,
+    pub y_pos: i32,
+    pub health: i32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -41,18 +28,18 @@ pub fn parse_players() -> HashMap<String, Player> {
 
     // Read or create the save file
     let unparsed_players = match read_to_string(&player_file_path) {
-        Ok(file) => file, 
+        Ok(file) => file,
         Err(err) => match err.kind() {
             ErrorKind::NotFound => {
                 File::create(&player_file_path).expect("Failed to create config file");
                 String::new()
-            },
+            }
             _ => panic!("Failed to create save file!"),
-        }
+        },
     };
 
     let mut players: HashMap<String, Player> = HashMap::new();
-    
+
     let re = Regex::new(r"\(([^:]+):(\d+):(\d+):(\d+)\)").unwrap();
     for cap in re.captures_iter(&unparsed_players) {
         let player = Player {
@@ -67,14 +54,17 @@ pub fn parse_players() -> HashMap<String, Player> {
     players
 }
 
-pub fn save_players(players: HashMap<String, Player>) { 
+pub fn save_players(players: &HashMap<String, Player>) {
     let mut player_file_path = home::home_dir().unwrap();
     player_file_path.push("Demo");
     player_file_path.push("players");
 
     let mut save_file = String::new();
     for player in players.values() {
-        let fmt_player = format!("({}:{}:{}:{})\n", player.name, player.health, player.x_pos, player.y_pos);
+        let fmt_player = format!(
+            "({}:{}:{}:{})\n",
+            player.name, player.health, player.x_pos, player.y_pos
+        );
         save_file.push_str(&fmt_player);
     }
     let mut file = std::fs::File::create(player_file_path).unwrap();
