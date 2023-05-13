efcc549b54f7acd272102499ffa16d033b456ac0
Slushee-a <slushee@duck.com>
2023-05-13 11:24:50 +0200

    Add playercount API call

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,6 +8,11 @@ use actix_web::{
 mod players;
 use players::{mod_players, parse_players, Field, Message, Player};
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Playercount(usize);
+
 #[get("/players")]
 async fn get_players(players: web::Data<HashMap<String, Player>>) -> impl Responder {
     // This is a bit jank, but the clock is ticking
@@ -16,9 +21,13 @@ async fn get_players(players: web::Data<HashMap<String, Player>>) -> impl Respon
         de_datafied_players.insert(k.to_string(), v.clone());
     }
 
-    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin", "*")).json(Message {
-        players: de_datafied_players
-    })
+    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin", "*")).json(de_datafied_players)
+}
+
+#[get("/playercount")]
+async fn playercount(players: web::Data<HashMap<String, Player>>) -> impl Responder {
+    println!("{}", players.len());
+    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin", "*")).json(Playercount(players.len()))
 }
 
 #[get("/update/{player}/{field}/{value}")]
@@ -37,7 +46,6 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-
     HttpServer::new(|| {
         let mut players: HashMap<String, Player> = HashMap::new();
         players.insert("Player1".to_string(), Player{
@@ -50,6 +58,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(players))
             .service(get_players)
             .service(update_player)
+            .service(playercount)
     })
     .bind("127.0.0.1:8080")?
     .run()
