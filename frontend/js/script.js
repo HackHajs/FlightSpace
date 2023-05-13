4d57f81f8d66348aba6486884b45a433f0394e35
Peipr <maro@peiphy.xyz>
2023-05-13 10:06:37 +0200

    Added a field, will beautify code now

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -2,57 +2,43 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const radius = 25;
 const speed = 50; // pixels per frame
-
-// set initial position
 let x = 50;
 let y = 50;
-
-// draw circle at initial position
+ctx.fillStyle = 'red';
+ctx.fillRect(0,0,640,550);
+ctx.fillStyle = 'blue';
+ctx.fillRect(640,0,640,550);
 ctx.beginPath();
 ctx.arc(x, y, radius, 0, 2 * Math.PI);
-ctx.fillStyle = 'blue';
+ctx.fillStyle = 'black';
 ctx.fill();
-
-// add event listener for mouse click
 canvas.addEventListener('click', (event) => {
     // get click coordinates relative to canvas
     const rect = canvas.getBoundingClientRect();
     const targetX = event.clientX - rect.left;
     const targetY = event.clientY - rect.top;
-
-    // calculate distance to target
+    console.log(event.clientX)
     const dx = targetX - x;
     const dy = targetY - y;
     const distance = Math.sqrt(dx*dx + dy*dy);
-
-    // calculate number of frames needed to reach target
     const frames = Math.ceil(distance / speed);
-
-    // calculate x and y increments per frame
     const incrementX = dx / frames;
     const incrementY = dy / frames;
-
-    // start animation
     let frame = 1;
     const animate = () => {
-        // clear canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // update position
+        ctx.fillStyle = 'red';
+        ctx.fillRect(0,0,640,550);
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(640,0,640,550);
+        ctx.fillStyle = 'black';
         x += incrementX;
         y += incrementY;
-
-        // draw circle at new position
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI);
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = 'black';
         ctx.fill();
-
-        // check if animation is finished
         if (frame < frames) {
             frame++;
-            requestAnimationFrame(animate);
-        }
-    };
-    animate();
-});
+            requestAnimationFrame(animate);}};
+    animate();});
