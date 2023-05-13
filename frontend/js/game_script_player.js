d136d5ee74dfd8ce6049bbf6737e91a9f6bf8a09
Peipr <maro@peiphy.xyz>
2023-05-13 18:42:39 +0200

    API

--- frontend/js/game_script_player.js
+++ frontend/js/game_script_player.js
@@ -5,13 +5,19 @@ const radius = 25;
 const speed = 50;
 let x = 640;
 let y = 275;
-console.log("GSP initted");
+
 //////////////////////////////////DRAW POINT/////////////////////////////////////////
 cta.beginPath();
 cta.arc(x, y, radius, 0, 2 * Math.PI);
 cta.fillStyle = '#5972D8'; cta.fill();
 //////////////////////////////////CLICKETY CLICK/////////////////////////////////////////
 canvis.addEventListener('click', (event) => {
+    //////////////////////////////////SEND API/////////////////////////////////////////
+    fetch('http://localhost:8080/update/'+uname+":ypos:"+y)
+        .catch(error => console.error(error));
+    fetch('http://localhost:8080/update/'+uname+":xpos:"+x)
+        .catch(error => console.error(error));
+    //////////////////////////////////(S)END API/////////////////////////////////////////
     console.log("clicc")
     const rect = canvis.getBoundingClientRect();
     const targetX = event.clientX - rect.left; const targetY = event.clientY - rect.top;
