2b472da5d89c46f437a77ffeda655e88d33cfafa
Peipr <maro@peiphy.xyz>
2023-05-14 02:21:48 +0200

    Most APIs done, still need lose condition

--- frontend/js/question.js
+++ frontend/js/question.js
@@ -1,8 +1,18 @@
 const cnvs = document.getElementById("quest");
 const cnv = cnvs.getContext("2d");
+var number=0;
 cnv.fillStyle = '#FFFFFF'; cnv.fillRect(0,0,640,100);
 cnv.fillStyle = '#000000'; cnv.font = "20px serif"; cnv.fillText("Are you an informed tourist? Let's check with this quick quiz!", 27, 50 , 600);
+/////////////////////////////////////////////////////////////////////////////////////////////////
 setInterval(function() {
+    if (number){
+    fetch('http://localhost:8080/judge/'+number)
+        .then(response => response.json())
+        .then(data => {
+            var question=data.question;
+            var number=data.number;
+            console.log("JUDGED");})
+        .catch(error => console.error(error));}
     fetch('http://localhost:8080/question')
         .then(response => response.json())
         .then(data => {
