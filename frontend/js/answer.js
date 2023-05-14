5a0a2fa068d2cf4f577d6638507e2fa9dea57194
Peipr <maro@peiphy.xyz>
2023-05-14 09:28:41 +0200

    Minor bugfix (rectangles not rendering)

--- frontend/js/answer.js
+++ frontend/js/answer.js
@@ -1,6 +1,6 @@
 const maro = document.getElementById("answer");
 const dmv = maro.getContext("2d");
-dmv.fillStyle = '#FFFFFF'; dmv.fillRect(0,0,683,100);
+dmv.fillStyle = '#FFFFFF'; dmv.fillRect(0,0,10000,100);
 console.log("ANSWER");
 /////////////////////////////////////////////////////////////////////////////////////////////////
 setInterval(function() {
@@ -11,10 +11,10 @@ setInterval(function() {
             var answerb = data.b;
             console.log(answera);
             dmv.clearRect(0, 0, maro.width, maro.height); dmv.font = "20px serif";
-            dmv.fillStyle = '#FFFFFF'; dmv.fillRect(0, 0, 640, 100);
+            dmv.fillStyle = '#FFFFFF'; dmv.fillRect(0, 0, 10000, 100);
             dmv.fillStyle = '#000000'; dmv.fillText(answera, 27, 35, 600);
             dmv.fillStyle = '#FFFFFF'; dmv.fillRect(640, 0, 640, 100);
-            dmv.fillStyle = '#000000'; dmv.fillText(answerb, 697, 35, 600);
+            dmv.fillStyle = '#000000'; dmv.fillText(answerb, 697, 35, 1000);
             questnum=questnum+1;
         })
         .catch(error => console.error(error)); // Log any errors to the console}, 10000);
--- frontend/js/bg.js
+++ frontend/js/bg.js
@@ -2,3 +2,4 @@ const canbas = document.getElementById('bg');
 const cth = canbas.getContext('2d');
 cth.fillStyle = '#FFCC00'; cth.fillRect(0,0,683,750);
 cth.fillStyle = '#4D4D4D'; cth.fillRect(683,0,683,750);
+cth.fillStyle = '#000000'; cth.fillText(health, 27, 35, 600);
